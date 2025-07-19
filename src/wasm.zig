252c1833bd016e8b494300a0f06a34d051b8f20b
rtb <beam@racingthebeam.dev>
2025-07-19 19:21:19 +0100

    update notify impl for latest Zig

--- src/wasm.zig
+++ src/wasm.zig
@@ -1,10 +1,12 @@
 const builtin = @import("builtin");
 const std = @import("std");
 
-pub usingnamespace if (builtin.is_test) struct {
+const NotifyImpl = if (builtin.is_test) struct {
     pub fn notifyBlockChanged(device_id: i32, block: u32) void {
         std.debug.print("Block changed {}:{}\n", .{ device_id, block });
     }
 } else struct {
     pub extern fn notifyBlockChanged(device_id: i32, block: u32) void;
 };
+
+pub const notifyBlockChanged = NotifyImpl.notifyBlockChanged;
