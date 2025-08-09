620c6f09a39b79d575df434e0f3a238385654a19
rtb <beam@racingthebeam.dev>
2025-08-10 00:03:59 +0100

    Remove out of date comment

--- src/limits.zig
+++ src/limits.zig
@@ -15,9 +15,6 @@ pub fn indirectThreshold(blk_size: u32) u32 {
 pub fn maxFileSize(blk_size: u32) u32 {
     const direct = indirectThreshold(blk_size);
     const indirect = ((blockPointersPerBlock(blk_size) / 2) * blockPointersPerBlock(blk_size) * blk_size);
-
-    // the current implementation automatically allocates a block
-    // when we hit the end so the max size is actually one byte less.
     return direct + indirect;
 }
 
